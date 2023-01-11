aaeea61f43e10a267885e93aa9fcaeda8196b04a
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-01-11 17:40:44 +0700

    chore: rename to multistepform-example

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,21 +1,21 @@
 /** @type {import('next-seo').DefaultSeoProps} */
 const defaultSEOConfig = {
-  title: "nextarter-chakra",
-  titleTemplate: "%s | nextarter-chakra",
-  defaultTitle: "nextarter-chakra",
-  description: "Next.js + chakra-ui + TypeScript template",
-  canonical: "https://nextarter-chakra.sznm.dev",
+  title: "multistepform-example",
+  titleTemplate: "%s | multistepform-example",
+  defaultTitle: "multistepform-example",
+  description: "Multi Step Form example",
+  canonical: "https://multistepform-example.sznm.dev",
   openGraph: {
-    url: "https://nextarter-chakra.sznm.dev",
-    title: "nextarter-chakra",
-    description: "Next.js + chakra-ui + TypeScript template",
+    url: "https://multistepform-example.sznm.dev",
+    title: "multistepform-example",
+    description: "Multi Step Form example",
     images: [
       {
-        url: "https://og-image.sznm.dev/**nextarter-chakra**.sznm.dev.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fsznm.dev%2Favataaars.svg&widths=250",
-        alt: "nextarter-chakra.sznm.dev og-image",
+        url: "https://og.sznm.dev/api/generate?heading=Multi%20Step%20Form&text=Multi%20Step%20Form%20example%20app%20with%20persisted%20state%20between%20session.%20Powered%20by%20react-hook-form%20+%20zustand&template=color&center=true",
+        alt: "multistepform-example.sznm.dev og-image",
       },
     ],
-    site_name: "nextarter-chakra",
+    site_name: "multistepform-example",
   },
   twitter: {
     handle: "@sozonome",
