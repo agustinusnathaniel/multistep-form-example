2d524a301afad0c41a09597721dc012aa08764dd
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-01-25 14:22:35 +0700

    feat: add refine for conditional validation

--- src/lib/models/form/item-form.ts
+++ src/lib/models/form/item-form.ts
@@ -2,10 +2,15 @@ import type { z } from "zod";
 
 import { submissionFormRequestScheme } from "lib/models/form/request";
 
-export const itemFormScheme = submissionFormRequestScheme.pick({
-  price: true,
-  categories: true,
-  description: true,
-});
+export const itemFormScheme = submissionFormRequestScheme
+  .pick({
+    price: true,
+    categories: true,
+    description: true,
+  })
+  .refine((val) => val.description === "hello" && val.price > 10, {
+    message: "Price must be > 10 if description is hello",
+    path: ["price"],
+  });
 
 export type ItemForm = z.infer<typeof itemFormScheme>;
