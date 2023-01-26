9e3965358ebc717113a293773d9e12c3da6c230d
Agustinus Nathaniel <berserk.nate@gmail.com>
2023-01-26 19:21:48 +0700

    refactor: use refine

--- src/lib/models/form-superstruct/item-form.ts
+++ src/lib/models/form-superstruct/item-form.ts
@@ -2,10 +2,18 @@ import * as ss from "superstruct";
 
 import { submissionFormRequestScheme } from "./request";
 
-export const itemFormScheme = ss.pick(submissionFormRequestScheme, [
-  "price",
-  "categories",
-  "description",
-]);
+export const itemFormScheme = ss.refine(
+  ss.pick(submissionFormRequestScheme, ["price", "categories", "description"]),
+  "itemFormScheme",
+  (val) => {
+    if (val.description === "hello" && val.price > 10) {
+      return {
+        message: "Price must be > 10 if description is hello",
+        path: ["price"],
+      };
+    }
+    return true;
+  }
+);
 
 export type ItemForm = ss.Infer<typeof itemFormScheme>;
--- src/lib/models/form-superstruct/request.ts
+++ src/lib/models/form-superstruct/request.ts
@@ -1,19 +1,38 @@
 import * as ss from "superstruct";
 
 export const submissionFormRequestScheme = ss.object({
-  name: ss.define<string>("name", (val: string) => {
+  name: ss.refine(ss.string(), "name", (val) => {
     if (val.length <= 1) {
       return { message: "Name must be filled" };
     }
     return true;
   }),
-  email: ss.define<string>("email", (email: string) => email.length > 0),
-  phoneNumber: ss.define<string>("phoneNumber", () => true),
-  age: ss.define<number>("age", () => true),
-  price: ss.define<number>("price", () => true),
-  description: ss.define<string | undefined>("description", () => true),
-  categories: ss.define<Array<string>>("categories", () => true),
-  inviteCode: ss.define<string | undefined>("inviteCode", () => true),
+  email: ss.refine(ss.string(), "email", (email) => email.length > 10),
+  phoneNumber: ss.refine(ss.string(), "phoneNumber", (val) =>
+    /^(^\+62|62|^08)(\d{3,4}-?){2}\d{3,4}$/g.test(val)
+  ),
+  age: ss.refine(ss.number(), "age", (val) => {
+    if (val < 1) {
+      return {
+        message: "Minimum 1",
+      };
+    }
+    if (val > 99) {
+      return {
+        message: "Maximum 99",
+      };
+    }
+    return true;
+  }),
+  price: ss.refine(ss.number(), "price", (val) => {
+    if (val < 1) {
+      return "Minimum 1";
+    }
+    return true;
+  }),
+  description: ss.refine(ss.optional(ss.string()), "description", () => true),
+  categories: ss.refine(ss.array(ss.string()), "categories", () => true),
+  inviteCode: ss.refine(ss.optional(ss.string()), "inviteCode", () => true),
 });
 
 export type SubmissionFormRequest = {
